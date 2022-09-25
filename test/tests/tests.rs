707c4c66a4d57a4c550afb2b203dc57ffe0421c2
Ralf Jung <post@ralfj.de>
2022-09-25 11:56:31 +0200

    add a test for an actual stdlib debug assertion

--- test/tests/tests.rs
+++ test/tests/tests.rs
@@ -1,11 +1,18 @@
 #![allow(deprecated, invalid_value)]
 
+use std::ffi::CString;
 use std::mem;
 
 #[test]
 #[should_panic]
 fn uninit_bool_array() {
     unsafe { mem::uninitialized::<[bool; 1]>() };
+}
+
+#[test]
+#[should_panic]
+fn c_str() {
+    unsafe { CString::from_vec_unchecked(vec![0]) };
 }
 
 // We cannot test get_unchecked bounds checking since that will abort, not unwind.
