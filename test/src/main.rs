da6521f86ba2ede54ebd3743a0d0f6b6d2d60e67
Ralf Jung <post@ralfj.de>
2023-02-04 20:36:28 +0100

    more abort-test-fixes

--- test/src/main.rs
+++ test/src/main.rs
@@ -2,8 +2,7 @@
 fn main() {
     eprintln!("expecting a panic...");
     std::panic::catch_unwind(|| unsafe {
-        // We want the super strict checks, so this should panic.
-        let _bad: u8 = std::mem::uninitialized();
+        unsafe { std::ffi::CString::from_vec_unchecked(vec![0]) };
     })
     .unwrap_err();
     eprintln!("... looking good!");
--- test/tests/tests.rs
+++ test/tests/tests.rs
@@ -11,6 +11,15 @@ fn uninit_bool_array() {
     unsafe { mem::uninitialized::<[bool; 1]>() };
 }
 
+// We cannot test `uninitialized` checking since that will abort, not unwind.
+#[test]
+#[ignore]
+#[should_panic]
+fn uninit_u8() {
+    // We want the super strict checks, so this should panic.
+    unsafe { std::mem::uninitialized::<u8>() };
+}
+
 #[test]
 #[should_panic]
 fn c_str() {
