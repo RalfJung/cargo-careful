13d26208a7caa3eb7d4b9d438e2377d4b19ed6d7
Ralf Jung <post@ralfj.de>
2023-08-30 15:24:38 +0200

    ci: make sure things are locked

--- test/ci.sh
+++ test/ci.sh
@@ -3,19 +3,19 @@ set -e
 
 # setup
 export RUSTFLAGS="-D warnings"
-cargo install --path ..
+cargo install --locked --path ..
 
 # test
 cargo careful setup
-cargo careful build
+cargo careful build --locked
 cargo clean
-cargo careful run
-cargo careful test
+cargo careful run --locked
+cargo careful test --locked
 
 # test no-std
 pushd test-no_std
 rustup target add x86_64-unknown-none
 cargo careful setup --target x86_64-unknown-none
-cargo careful build --target x86_64-unknown-none
+cargo careful build --target x86_64-unknown-none --locked
 cargo clean
 popd
