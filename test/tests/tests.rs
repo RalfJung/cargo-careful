bdbad39d034a1bab8d3f799fd2902326c382809a
Ralf Jung <post@ralfj.de>
2024-05-19 14:16:21 +0200

    update motivating example

--- test/tests/tests.rs
+++ test/tests/tests.rs
@@ -20,6 +20,18 @@ fn uninit_u8() {
     let _ = unsafe { std::mem::uninitialized::<u8>() };
 }
 
+// We cannot test `read` alignment checks since that will abort, not unwind.
+#[test]
+#[ignore]
+#[should_panic]
+fn read_unaligned() {
+    let arr = [1u8, 2, 3, 4];
+    for n in [0, 1] {
+        let val = unsafe { arr.as_ptr().add(n).cast::<u16>().read() };
+        println!("The value is {val}!");
+    }
+}
+
 #[test]
 #[should_panic]
 fn c_str() {
